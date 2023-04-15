0cc54cdcf4fcf4864312a7ffc8342a568d20febc
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2023-04-15 20:19:10 +0300

    Fixed the bug: commented the Config-class in the Event-model. Changed host and port of the server.

--- models/events.py
+++ models/events.py
@@ -11,13 +11,13 @@ class Event(BaseModel):
     tags: List[str]
     location: str
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "title": "FastAPI Book Launch",
-                "image": "https://linktomyimage.com/image.png",
-                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
-                "tags": ["python", "fastapi", "book", "launch"],
-                "location": "Google Meet"
-            }
-        }
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "title": "FastAPI Book Launch",
+    #             "image": "https://linktomyimage.com/image.png",
+    #             "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+    #             "tags": ["python", "fastapi", "book", "launch"],
+    #             "location": "Google Meet"
+    #         }
+    #     }
