2e37537ea9f6f820cc3f9c6c9d708090fde9adce
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2023-04-23 19:03:12 +0300

    Started to deploy the app locally.

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ async def init_db():
 
 # Run server
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
